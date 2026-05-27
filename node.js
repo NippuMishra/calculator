a641596a5537947718eea8cdfa1aa04c7a3e2e1c
nippu <nippu3670@gmail.com>
2026-05-27 18:22:43 +0530

    testing my memmory

--- node.js
+++ node.js
@@ -6,3 +6,5 @@ console.log(a * b) // Output: 6
 console.log(a / b) // Output: 0.6666666666666666
 console.log(a - b) // Output: -1
 console.log(a ** b) // Output: 8
+console.log(a % b) // Output: 2
+    
